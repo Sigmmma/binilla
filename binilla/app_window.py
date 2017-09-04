1b8312dc442cfba4f9a8d154c521ab68de9bcc75
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2017-09-04 17:50:11 -0400

    version bump

--- binilla/app_window.py
+++ binilla/app_window.py
@@ -125,7 +125,7 @@ class Binilla(tk.Tk, BinillaWidget):
     '''Miscellaneous properties'''
     _initialized = False
     app_name = "Binilla"  # the name of the app(used in window title)
-    version = '0.9.30'
+    version = '0.9.31'
     log_filename = 'binilla.log'
     debug = 0
     debug_mode = False

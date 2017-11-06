606441e0d90c68058d8079e7f58e343974f88694
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2017-11-05 20:35:00 -0500

    Fixed bug where if None was provided as the tag in delete_tag it would exception

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ except ImportError:
 curr_dir = dirname(__file__)
 
 #               YYYY.MM.DD
-release_date = "2017.10.27"
-version = (0, 9, 41)  # DONT FORGET TO UPDATE THE VERSION IN app_window.py
+release_date = "2017.11.05"
+version = (0, 9, 42)  # DONT FORGET TO UPDATE THE VERSION IN app_window.py
 
 try:
     try:

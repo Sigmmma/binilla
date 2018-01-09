68b5e30712a853ffdfa44912032236d6eefab5c3
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2018-01-08 21:05:00 -0500

    Fixed the indent bug by changing how indention is handeled Fixed bug with text box size

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ except ImportError:
 curr_dir = dirname(__file__)
 
 #               YYYY.MM.DD
-release_date = "2018.01.06"
-version = (0, 9, 50)  # DONT FORGET TO UPDATE THE VERSION IN app_window.py
+release_date = "2018.01.08"
+version = (0, 9, 51)  # DONT FORGET TO UPDATE THE VERSION IN app_window.py
 
 try:
     try:

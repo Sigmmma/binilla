080e9c1a9aa581d0d141d621918f2d6130e59bdf
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2018-01-20 22:27:02 -0500

    Getting tired or fixing scroll bugs. Assuming this breaks something on linux...

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ curr_dir = dirname(__file__)
 
 #               YYYY.MM.DD
 release_date = "2018.01.20"
-version = (0, 9, 54)  # DONT FORGET TO UPDATE THE VERSION IN app_window.py
+version = (0, 9, 55)  # DONT FORGET TO UPDATE THE VERSION IN app_window.py
 
 try:
     try:

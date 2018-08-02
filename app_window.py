e1ef69586e95a77c0f55f591fda6b5153fc7a502
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2018-08-02 03:48:03 -0400

    Version bump

--- app_window.py
+++ app_window.py
@@ -100,7 +100,7 @@ class Binilla(tk.Tk, BinillaWidget):
     '''Miscellaneous properties'''
     _initialized = False
     app_name = "Binilla"  # the name of the app(used in window title)
-    version = '0.9.61'
+    version = '0.9.62'
     log_filename = 'binilla.log'
     debug = 0
     debug_mode = False
--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ except ImportError:
 curr_dir = dirname(__file__)
 
 #               YYYY.MM.DD
-release_date = "2018.07.05"
-version = (0, 9, 61)  # DONT FORGET TO UPDATE THE VERSION IN app_window.py
+release_date = "2018.08.03"
+version = (0, 9, 62)  # DONT FORGET TO UPDATE THE VERSION IN app_window.py
 
 try:
     try:

ae684ffe34440c8edafdd3c2d56b6b9c77f4094a
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2018-09-26 04:26:48 -0400

    Undoing some stupid non-optimizations

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ except ImportError:
 curr_dir = dirname(__file__)
 
 #               YYYY.MM.DD
-release_date = "2018.09.01"
-version = (0, 9, 64)  # DONT FORGET TO UPDATE THE VERSION IN app_window.py
+release_date = "2018.09.26"
+version = (0, 9, 65)  # DONT FORGET TO UPDATE THE VERSION IN app_window.py
 
 try:
     try:

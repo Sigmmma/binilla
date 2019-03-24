cfcf8f89e6cfc937e41d7d33680f320afbb6c873
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2019-03-23 21:22:56 -0400

    Version bump Fixed incorrect widget binding

--- __init__.py
+++ __init__.py
@@ -3,8 +3,8 @@
 # ##############
 __author__ = "Devin Bobadilla"
 #           YYYY.MM.DD
-__date__ = "2019.03.21"
-__version__ = (1, 0, 0)
+__date__ = "2019.03.23"
+__version__ = (1, 0, 1)
 __all__ = (
     'app_window', 'config_def', 'constants', 'edit_manager',
     'editor_constants', 'field_widgets', 'handler',

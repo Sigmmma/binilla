c6354dfe444f4165ac58c193350fb2d606b4b964
Moses_of_Egypt <Moses_of_Egypt@localhost>
2019-07-10 21:16:32 -0400

    Splitting tooltips into separate modules

---
+++ defs/style_tooltips.py
@@ -0,0 +1,60 @@
+# widths and heights
+title_width = (
+    "Number of characters wide the title of each vertically oriented field is.")
+scroll_menu_width = (
+    "Default number of characters wide an enumerator widget will be when\n" +
+    "not being used to represent an enumerator(such as in an array or union)")
+enum_menu_width = (
+    "Default number of characters wide an enumerator widget will be.")
+min_entry_width = (
+    "Minimum number of characters wide an entry field must be.")
+
+textbox_max_width = "Width of multi-line text boxes"
+textbox_max_height = "Height of multi-line text boxes"
+
+scroll_menu_max_width = (
+    "Maximum number of characters wide an enumerator widget can be.\n" +
+    "(This is regardless of what the enumerator widget is being used for)")
+scroll_menu_max_height = (
+    "Maximum number of characters tall an enumerator widget can be.\n" +
+    "(This is regardless of what the enumerator widget is being used for)")
+
+bool_frame_min_width  = "Minimum number of pixels wide a boolean frame must be."
+bool_frame_max_width  = "Maximum number of pixels wide a boolean frame can be."
+bool_frame_min_height = "Minimum number of pixels tall a boolean frame must be."
+bool_frame_max_height = "Maximum number of pixels tall a boolean frame can be."
+
+default_integer_entry_width  = (
+    "Default number of characters wide an integer entry field will be.")
+default_float_entry_width  = (
+    "Default number of characters wide an float entry field will be.")
+default_string_entry_width  = (
+    "Default number of characters wide an string entry field will be.")
+
+max_integer_entry_width  = (
+    "Maximum number of characters wide an integer entry field can be.")
+max_float_entry_width  = (
+    "Maximum number of characters wide an float entry field can be.")
+max_string_entry_width  = (
+    "Maximum number of characters wide an string entry field can be.")
+
+# depth
+widget_depth = (
+    "Number of pixels to surround the widget with to give an appearance of depth.")
+
+# padding
+pad_str = "Padding applied to the %s of widgets oriented %sally"
+vertical_padx = pad_str % ('left/right', 'vertic')
+vertical_pady = pad_str % ('top/bottom', 'vertic')
+horizontal_padx = pad_str % ('left/right', 'horizont')
+horizontal_pady = pad_str % ('top/bottom', 'horizont')
+
+vertical_padx_l = pad_str % ('left', 'vertic')
+vertical_padx_r = pad_str % ('right', 'vertic')
+vertical_pady_l = pad_str % ('top', 'vertic')
+vertical_pady_r = pad_str % ('bottom', 'vertic')
+
+horizontal_padx_l = pad_str % ('left', 'horizont')
+horizontal_padx_r = pad_str % ('right', 'horizont')
+horizontal_pady_l = pad_str % ('top', 'horizont')
+horizontal_pady_r = pad_str % ('bottom', 'horizont')

ef639972d71237ccbccd42993b7161392eecaa89
Moses_of_Egypt <Moses_of_Egypt@localhost>
2019-08-04 16:52:15 -0400

    Version bump

--- __init__.py
+++ __init__.py
@@ -3,8 +3,8 @@
 # ##############
 __author__ = "Devin Bobadilla"
 #           YYYY.MM.DD
-__date__ = "2019.07.05"
-__version__ = (1, 1, 0)
+__date__ = "2019.08.04"
+__version__ = (1, 1, 1)
 __all__ = (
     'defs', 'widgets', 'windows',
     'app_window', 'constants', 'edit_manager', 'editor_constants', 'handler',

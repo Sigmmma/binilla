d71eae79783d530bf6432b5318881fc7137c7c53
Moses_of_Egypt <mosesbobadilla@gmail.com>
2019-09-05 14:55:39 -0400

    Version bump

--- binilla/__init__.py
+++ binilla/__init__.py
@@ -3,8 +3,8 @@
 # ##############
 __author__ = "Devin Bobadilla"
 #           YYYY.MM.DD
-__date__ = "2019.08.10"
-__version__ = (1, 1, 2)
+__date__ = "2019.09.05"
+__version__ = (1, 1, 3)
 __all__ = (
     'defs', 'widgets', 'windows',
     'app_window', 'constants', 'edit_manager', 'editor_constants', 'handler',

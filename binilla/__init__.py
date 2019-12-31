6a0aef5bec46c8c744194e6ece5991503a91004a
gbMichelle <gbmichelle.dev@gmail.com>
2019-12-31 03:29:54 +0100

    [meta] Update version for requirements. Update requirements versions.

--- binilla/__init__.py
+++ binilla/__init__.py
@@ -3,8 +3,8 @@
 # ##############
 __author__ = "Devin Bobadilla"
 #           YYYY.MM.DD
-__date__ = "2019.09.05"
-__version__ = (1, 1, 3)
+__date__ = "2020.01.01"
+__version__ = (1, 2, 0)
 __all__ = (
     'defs', 'widgets', 'windows',
     'app_window', 'constants', 'edit_manager', 'editor_constants', 'handler',

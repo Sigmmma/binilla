d3ece03e30010c8b1d0246ad2b7dac1dfb3440e0
gbMichelle <gbmichelle.dev@gmail.com>
2019-12-31 03:29:54 +0100

    [meta] Update version for requirements. Update requirements versions.

--- setup.py
+++ setup.py
@@ -44,9 +44,9 @@ setup(
     # arbytmap can be removed from the dependencies if you cannot install
     # it for some reason, though it will prevent certain things from working.
     install_requires=[
-        'supyr_struct', 'arbytmap', 'threadsafe_tkinter', 'tkcolorpicker'],
+        'supyr_struct>=1.4.0', 'arbytmap', 'threadsafe_tkinter', 'tkcolorpicker'],
     requires=[
-        'supyr_struct', 'arbytmap', 'threadsafe_tkinter', 'tkcolorpicker'],
+        'supyr_struct>=1.4.0', 'arbytmap', 'threadsafe_tkinter', 'tkcolorpicker'],
     provides=['binilla'],
     classifiers=[
         "Development Status :: 4 - Beta",

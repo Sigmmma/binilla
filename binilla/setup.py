06e942eb1a9e629027e9c247fc629e9c718c48be
gbMichelle <gbmichelle.dev@gmail.com>
2020-01-21 00:46:36 +0100

    Update setup.py

--- binilla/setup.py
+++ binilla/setup.py
@@ -1,21 +1,12 @@
 #!/usr/bin/env python
-from os.path import dirname, join
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
-curr_dir = dirname(__file__)
-
 import binilla
 
-try:
-    try:
-        long_desc = open(join(curr_dir, "readme.rst")).read()
-    except Exception:
-        long_desc = open(join(curr_dir, "readme.md")).read()
-except Exception:
-    long_desc = 'Could not read long description from readme.'
+long_desc = open(join(curr_dir, "README.RST")).read()
 
 setup(
     name='binilla',
@@ -34,16 +25,15 @@ setup(
         'binilla.windows',
         ],
     package_data={
-        '': ['*.txt', '*.md', '*.rst', '*.pyw'],
         'binilla': [
-            'styles/*.*',
+            'styles/*.*', '*.[tT][xX][tT]', '*.[mM][dD]', '*.RST', '*.pyw'
             ]
         },
     platforms=["POSIX", "Windows"],
     keywords="binilla, binary, data structure",
     # arbytmap can be removed from the dependencies if you cannot install
     # it for some reason, though it will prevent certain things from working.
-    install_requires=['supyr_struct>=1.4.0', 'arbytmap', 'threadsafe_tkinter',
+    install_requires=['supyr_struct', 'arbytmap', 'threadsafe_tkinter',
                       'tkcolorpicker'],
     requires=['supyr_struct', 'arbytmap', 'threadsafe_tkinter',
               'tkcolorpicker'],
@@ -51,9 +41,15 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3 :: Only",
         ],
     zip_safe=False,
     )

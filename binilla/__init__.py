6a486c8414b95081c747f4e36e92b778bb34ef44
gbMichelle <gbmichelle.dev@gmail.com>
2020-02-07 02:30:29 +0100

    Install supyr_struct before building because of the stupid __init__.py faaaaaa

--- binilla/__init__.py
+++ binilla/__init__.py
@@ -5,7 +5,7 @@ __author__ = "Devin Bobadilla, Michelle van der Graaf"
 #           YYYY.MM.DD
 __date__ = "2020.01.29"
 __version__ = (1, 2, 3)
-__website__ = "https://github.com/MosesofEgypt/binilla"
+__website__ = "https://github.com/Sigmmma/binilla"
 __all__ = (
     'defs', 'widgets', 'windows',
     'app_window', 'constants', 'edit_manager', 'editor_constants', 'handler',

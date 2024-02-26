74e507597b337a146e5a55c06775a62624673fe3
Steven Garcia <stevengarcia55@ymail.com>
2024-02-26 01:34:23 -0600

    bump version

--- binilla/__init__.py
+++ binilla/__init__.py
@@ -3,8 +3,8 @@
 # ##############
 __author__ = "Sigmmma"
 #           YYYY.MM.DD
-__date__ = "2024.02.20"
-__version__ = (1, 3, 12)
+__date__ = "2024.02.26"
+__version__ = (1, 3, 13)
 __website__ = "https://github.com/Sigmmma/binilla"
 __all__ = (
     'defs', 'widgets', 'windows',
